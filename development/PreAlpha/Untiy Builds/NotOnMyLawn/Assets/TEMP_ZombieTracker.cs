81ae2290c67d920b4741d6ba07eef90a0c2dc520
ben <bmfboom9@gmail.com>
2023-06-14 19:52:40 -0600

    created a script but my laptop cant run unity lol

---
+++ development/PreAlpha/Untiy Builds/NotOnMyLawn/Assets/TEMP_ZombieTracker.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMP_ZombieTracker : MonoBehaviour
+{
+
+    public GameObject 2DCounterpart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
