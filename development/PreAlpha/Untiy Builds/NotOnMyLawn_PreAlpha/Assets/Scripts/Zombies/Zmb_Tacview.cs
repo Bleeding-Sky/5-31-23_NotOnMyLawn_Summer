37aa140d4be0c992c21defba5526db56eef3213c
ben <bmfboom9@gmail.com>
2023-07-17 17:29:50 -0600

    implemented cross scene tracking

--- development/PreAlpha/Untiy Builds/NotOnMyLawn_PreAlpha/Assets/Scripts/Zombies/Zmb_Tacview.cs
+++ development/PreAlpha/Untiy Builds/NotOnMyLawn_PreAlpha/Assets/Scripts/Zombies/Zmb_Tacview.cs
@@ -6,17 +6,31 @@ public class Zmb_Tacview : MonoBehaviour
 {
     public Zmb_Master masterScript;
 
+    public Vector2 targetPos;
+    public float xDisplacementFromTarget;
+    public float distanceFromTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //TODO: make window read pos from tacview
+        CalculateDisplacements();
     }
+
+    void CalculateDisplacements()
+    {
+        Vector3 myPos = transform.position;
+        xDisplacementFromTarget = myPos.x - targetPos.x;
+        distanceFromTarget = myPos.y - targetPos.y;
+    }
+
+    
 
     /// <summary>
     /// sends damage value to master script
--- development/PreAlpha/Untiy Builds/NotOnMyLawn_PreAlpha/Assets/Scripts/Zombies/Zmb_Window.cs
+++ development/PreAlpha/Untiy Builds/NotOnMyLawn_PreAlpha/Assets/Scripts/Zombies/Zmb_Window.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Zmb_Window : MonoBehaviour
 {
     public Zmb_Master masterScript;
     public Zmb_Tacview tacviewScript;
+
+    public Vector3 targetPos;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,18 @@ public class Zmb_Window : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //TODO: make window read pos from tacview
+        MatchTacviewPos();
+    }
+
+    void MatchTacviewPos()
+    {
+        float tacviewXDisplacement = tacviewScript.xDisplacementFromTarget;
+        float tacviewDistanceFromTarget = tacviewScript.distanceFromTarget;
+
+        float myXDisplacement = targetPos.x + tacviewXDisplacement;
+        float myZDisplacement = targetPos.z + tacviewDistanceFromTarget;
+
+        transform.position = new Vector3(myXDisplacement, targetPos.y, myZDisplacement);
     }
 
     /// <summary>
