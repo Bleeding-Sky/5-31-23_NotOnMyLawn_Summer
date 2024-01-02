d9517e2b2bf2bfd6aafb033f1386ce1c0b76dac7
ben <bmfboom9@gmail.com>
2024-01-02 12:48:03 -0700

    imported navigation package and integrated overhead pathfinding into demo scene

---
+++ Unity Projects/LastOneLeft_v1/Assets/_Zombie/OverheadPathing_Zombie.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OverheadPathing_Zombie : MonoBehaviour
+{
+    [Header("CONFIG")]
+    public Transform target;
+
+    [Header("DEBUG")]
+    public NavMeshAgent myAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fetch and configure agent component
+        myAgent = GetComponent<NavMeshAgent>();
+        myAgent.updateRotation = false;
+        myAgent.updateUpAxis = false;
+
+        //enable agent after configuration is complete to avoid errors
+        myAgent.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set destination to the position of the target every frame
+        myAgent.SetDestination(target.position);
+    }
+}
