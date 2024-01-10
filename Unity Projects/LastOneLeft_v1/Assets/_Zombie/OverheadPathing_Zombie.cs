cc166e5cdfd89e47eead94844e1b1862e033ad80
ben <bmfboom9@gmail.com>
2024-01-09 17:00:27 -0700

    made overhead pathing require a navmeshagent component

--- Unity Projects/LastOneLeft_v1/Assets/_Zombie/OverheadPathing_Zombie.cs
+++ Unity Projects/LastOneLeft_v1/Assets/_Zombie/OverheadPathing_Zombie.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
+
 public class OverheadPathing_Zombie : MonoBehaviour
 {
     [Header("CONFIG")]
