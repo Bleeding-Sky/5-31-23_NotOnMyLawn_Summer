cc96015f4c667d33deee35c4aba358f036d05671
ben <bmfboom9@gmail.com>
2024-01-18 09:54:05 -0700

    began work on overhead zombie statuses

---
+++ Unity Projects/LastOneLeft_v1/Assets/_Zombie/OverheadBehavior_Zombie.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OverheadBehavior_Zombie : MonoBehaviour
+{
+    [Header("CONFIG")]
+
+    [Header("DEBUG")]
+    public Status_Zombie zombieStates;
+    public bool isCrawling;
+    public bool isStunned;
+    public bool isStumbling;
+    public bool isFallen;
+    public bool isBurning;
+    public bool isEnraged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
