6b38e56f0c589efc7dfcc8106c67ac2771e79f30
Marinrp7344 <marinrp7344@gmail.com>
2024-06-09 01:22:30 -0600

    Finished Inventory selection, highlighting, swapping, and radial menu

---
+++ Unity Projects/LastOneLeft_v1/Assets/RadialItem_UI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RadialItem_UI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,IPointerEnterHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
