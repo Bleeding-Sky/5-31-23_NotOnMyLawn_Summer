e980a8ffe4c05a38312264bc387b9de3dad6606d
ben <bmfboom9@gmail.com>
2024-06-13 10:13:34 -0600

    started work on decal based blood system

---
+++ Unity Projects/LastOneLeft_v1/Assets/_Scripts/Enemy/Bleed2D_Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bleed2D_Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnBloodDecal(Vector2 incomingShotDirection)
+    {
+
+    }
+
+}
